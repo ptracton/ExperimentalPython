0ae41a9ac5dc3ef4ba465403a5180259a4b8903b
Phil Tracton <ptracton@gmail.com>
2018-04-08 13:44:53 -0700

    building a simple UI template so I can quickly build PyQT5 applications

---
+++ PyQT5/template/UI_Widget.py
@@ -0,0 +1,18 @@
+import PyQt5
+import PyQt5.QtCore
+import PyQt5.QtWidgets
+
+
+class UI_Widget(PyQt5.QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(UI_Widget, self).__init__(parent)
+        vbox = PyQt5.QtWidgets.QVBoxLayout()
+
+        label = PyQt5.QtWidgets.QLabel("Simple UI Template")
+        self.button = PyQt5.QtWidgets.QPushButton("Simple UI Button")
+
+        vbox.addWidget(label)
+        vbox.addWidget(self.button)
+        
+        self.setLayout(vbox)
+        return
